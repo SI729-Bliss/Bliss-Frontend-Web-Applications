ab8328d23ad6184aef1315109cc96640a4e24fd9
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2024-09-23 23:29:48 -0500

    chore: add toolbar with a language switcher component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { HeaderContentComponent } from './public/components/header-content/header-content.component';
+import { TranslateService } from "@ngx-translate/core";
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Bliss-Frontend-Web-Application';
+
+  constructor(translate: TranslateService) {
+    translate.setDefaultLang('en');
+    translate.use('en');
+  }
 }
---
+++ src/app/public/components/header-content/header-content.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { LanguageSwitcherComponent } from "../language-switcher/language-switcher.component";
+import { TranslateModule } from "@ngx-translate/core";
+
+@Component({
+  selector: 'app-header-content',
+  standalone: true,
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, LanguageSwitcherComponent, TranslateModule],
+  templateUrl: './header-content.component.html',
+  styleUrl: './header-content.component.css'
+})
+export class HeaderContentComponent {
+
+}
