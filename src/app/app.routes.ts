0d35d7d4ff1b2f5876a13464e8054140c858c3d7
ElviaRV <elviamarcela14@gmail.com>
2024-09-24 14:20:02 -0500

    feat(review-component):"Crud for user reviews added"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule} from '@angular/router';
+import {
+  ReviewCreateAndEditComponent,
+} from './review/components/review-create-and-edit/review-create-and-edit.component';
+import {ReviewListComponent} from "./review/components/review-list/review-list.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ReviewCreateAndEditComponent },
+  { path: 'reviews', component: ReviewListComponent },
+];
