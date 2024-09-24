5f7b94fc731d734416cc5eb8e33f60b7de629bfc
diego5m <sorianodiego2020@gmail.com>
2024-09-24 13:38:26 -0500

    feat: services configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// Imports of enterprise services
+
+import { HomeEnterpriseComponent } from "./enterprise/services/pages/home-enterprise/home-enterprise.component";
+import { ServicesManagementComponent } from "./enterprise/services/pages/services-management/services-management.component";
+
+
+export const routes: Routes = [
+  { path: 'home', component: HomeEnterpriseComponent },
+  { path: 'services', component: ServicesManagementComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+];
