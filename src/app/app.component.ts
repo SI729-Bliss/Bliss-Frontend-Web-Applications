fd5f70d1a4d2c02eafbfcdeaee3e7225d6d35bf3
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2024-10-08 23:06:55 -0500

    refactor: restructured code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,58 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {
-  CustomerCreateAndEditComponent
-} from "./customers/profiles/components/customer-create-and-edit/customer-create-and-edit.component";
-import {HeaderContentComponent} from "./public/components/header-content/header-content.component";
+import { CustomerCreateAndEditComponent } from "./customers/profiles/components/customer-create-and-edit/customer-create-and-edit.component";
+import { HeaderContentComponent } from "./public/components/header-content/header-content.component";
 import { TranslateService } from "@ngx-translate/core";
-
-import { OnInit, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
-import { MatDividerModule } from "@angular/material/divider";
-import { MatListModule } from "@angular/material/list";
-import { BreakpointObserver } from "@angular/cdk/layout";
-import {LanguageSwitcherComponent} from "./public/components/language-switcher/language-switcher.component";
-
-
+import { LanguageSwitcherComponent } from "./public/components/language-switcher/language-switcher.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, MatToolbarModule, MatButtonModule, MatIconModule,
-    MatSidenavModule, MatDividerModule, MatListModule, LanguageSwitcherComponent, HeaderContentComponent, CustomerCreateAndEditComponent],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    LanguageSwitcherComponent,
+    HeaderContentComponent,
+    CustomerCreateAndEditComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
   title = 'Bliss-Frontend-Web-Application';
 
-  @ViewChild(MatSidenav, {static: true}) sidenav!: MatSidenav;
   options = [
-    { icon: 'person', path: '/customer', title: 'Customers'},
-    { icon: 'person', path:'/company', title: 'Companies'},
+    { icon: 'person', path: '/customer', title: 'Customers' },
+    { icon: 'person', path: '/company', title: 'Companies' }
   ];
 
-  constructor(private translate: TranslateService, private observer: BreakpointObserver) {
+  constructor(private translate: TranslateService) {
     translate.setDefaultLang('en');
     translate.use('en');
   }
 
-
-
   ngOnInit(): void {
-    this.observer.observe(['(max-width: 1280px)'])
-      .subscribe((response) => {
-        if (response.matches) {
-          this.sidenav.mode = 'over';
-          this.sidenav.close();
-        } else {
-          this.sidenav.mode = 'side';
-          this.sidenav.open();
-        }
-      });
+    // Lógica de inicialización adicional si es necesaria
   }
 }
