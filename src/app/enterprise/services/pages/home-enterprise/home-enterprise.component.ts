ebddc464181b54b75fb5ed23a31b8268037e0217
diego5m <sorianodiego2020@gmail.com>
2024-10-15 13:50:29 -0500

    feat: update enterprise home

--- src/app/enterprise/services/pages/home-enterprise/home-enterprise.component.ts
+++ src/app/enterprise/services/pages/home-enterprise/home-enterprise.component.ts
@@ -4,32 +4,34 @@ import { EntsevicesService } from "../../services/entsevices.service";
 import { Entservice } from "../../model/entservice.entity";
 import { ServiceCardComponent } from "../../components/service-card/service-card.component";
 import {NgForOf} from "@angular/common";
+import {MatAnchor} from "@angular/material/button";
+import {RouterLink} from "@angular/router";
+import {TranslateModule} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-home-enterprise',
   standalone: true,
-  imports: [ServiceCardComponent, NgForOf],
+  imports: [ServiceCardComponent, NgForOf, MatAnchor, RouterLink, TranslateModule],
   templateUrl: './home-enterprise.component.html',
   styleUrl: './home-enterprise.component.css'
 })
 export class HomeEnterpriseComponent implements OnInit{
 
-  entserviceData: any;
+  entserviceData: any[];
   entservices: any[];
   allEntservices: any[];
 
   constructor(private entservicesService: EntsevicesService) {
 
-    this.entserviceData = {};
+    this.entserviceData = [];
     this.entservices = [];
     this.allEntservices = [];
   }
 
   private getAllEntservices(): void {
     this.entservicesService.getAll().subscribe((response: any) => {
-      console.log(response);
-      this.entservices = response;
-      console.log("Entservices:", this.entservices);
+      this.entserviceData = response;
+      console.log("EntservicesData:", this.entserviceData);
     })
   }
 
