010aa4c3dc6caecafc6adbc99854bb580ab2ee5f
David Alejandro Rivas Sarango <u20191e831@upc.edu.pe>
2024-10-15 12:19:17 -0500

    fixed: design

--- src/app/customers/home-client/components/service-card/service-card.component.ts
+++ src/app/customers/home-client/components/service-card/service-card.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ChangeDetectionStrategy} from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
@@ -10,12 +10,16 @@ import { NgClass } from "@angular/common";
 import {Service} from "../../model/service.entity";
 import { ServicesService } from '../../services/services.service';
 
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
 @Component({
   selector: 'app-service-card',
   standalone: true,
-  imports: [MatCardModule,  MatInputModule, MatFormFieldModule,CommonModule],
+  imports: [MatCardModule,  MatInputModule, MatFormFieldModule,CommonModule, MatButtonModule, MatIconModule],
   templateUrl: './service-card.component.html',
-  styleUrl: './service-card.component.css'
+  styleUrl: './service-card.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ServiceCardComponent implements OnInit{
 
