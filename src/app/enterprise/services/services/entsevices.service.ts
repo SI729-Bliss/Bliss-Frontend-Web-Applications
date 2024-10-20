de8df76ab5f84366861903734f11f0f369d7fee9
diego5m <sorianodiego2020@gmail.com>
2024-10-20 13:37:17 -0500

    feat: update entservices

--- src/app/enterprise/services/services/entsevices.service.ts
+++ src/app/enterprise/services/services/entsevices.service.ts
@@ -3,14 +3,27 @@ import { Injectable } from '@angular/core';
 import { BaseService } from "../../../shared/services/base.service";
 import { HttpClient } from "@angular/common/http";
 import { Entservice } from "../model/entservice.entity";
+import {catchError, Observable, retry} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class EntsevicesService extends BaseService<Entservice>{
 
-  constructor(http: HttpClient) {
+  salonContextId: number;
+
+  constructor(http: HttpClient, private httpForEnt: HttpClient) {
     super(http);
     this.resourceEndpoint = '/services';
+    this.salonContextId = 0;
+  }
+
+  getSalonContextId(): number {
+    return this.salonContextId;
+  }
+
+  getAllServicesBySalonId(): Observable<Entservice> {
+    return this.httpForEnt.get<Entservice>(`${this.basePath}${this.resourceEndpoint}?beauty_salon_id=${ this.salonContextId }`, this.httpOptions)
+      .pipe(retry(2), catchError(this.handleError));
   }
 }
