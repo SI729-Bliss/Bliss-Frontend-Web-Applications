36476f8765cf0b7fb8e18d181d1e6b926c3e7eb3
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2024-10-20 14:45:28 -0500

    chore: implemented enviroments

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -4,7 +4,7 @@ export const environment = {
 // Server Base Path for Fake REST API
  // serverBasePath: 'https://my-json-server.typicode.com/SI729-Bliss/db-server'
 // Server Base Path for Spring Boot REST API
-serverBasePath: 'http://localhost:3000/api/v1'
+serverBasePath: 'http://localhost:3000'
 
 };
 =======
