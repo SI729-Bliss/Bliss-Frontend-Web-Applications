7a9d2ef49a3c35457648c92d4045b248f86e219e
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2024-10-21 06:31:01 -0500

    refactor: deleted unused imports and code lines

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { HomeComponent } from './customers/home-client/pages/home/home.component';
-import {RouterModule, Routes} from '@angular/router';
+import { Routes} from '@angular/router';
 import { PageNotFoundComponent } from "./public/pages/page-not-found/page-not-found.component";
 import {ServicesHistoryComponent} from "./history/pages/services-history/services-history.component";
 import {ReviewPageComponent} from "./review/pages/review-page/review-page.component";
-import {NgModule} from "@angular/core";
 import { BookingComponent } from "./enterprise/services/pages/booking/booking.component";
 import { CitasListComponent } from "./enterprise/services/components/booking-card/booking-card.component";
 
@@ -12,31 +11,24 @@ import { ServicesManagementComponent } from "./enterprise/services/pages/service
 import { CustomerCreateAndEditComponent } from './customers/profiles/components/customer-create-and-edit/customer-create-and-edit.component';
 import { CompanyCreateAndEditComponent } from './companies/profiles/components/company-create-and-edit/company-create-and-edit.component';
 
-export class AppRoutingModule { }
 
 export const routes: Routes = [
+
   { path: 'catalog', component: HomeComponent },
   { path: 'my-services', component: ServicesHistoryComponent },
-  { path: 'review-page/:id', component: ReviewPageComponent },
-  { path: 'review-page', component: ReviewPageComponent },
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'booking', component: BookingComponent },
   { path: 'citas', component: CitasListComponent },
-  { path: '**', component: PageNotFoundComponent },
-
   { path: 'homeEnterprise', component: HomeEnterpriseComponent },
+
+  { path: 'review-page/:id', component: ReviewPageComponent },
+  { path: 'review-page', component: ReviewPageComponent },
   { path: 'services', component: ServicesManagementComponent },
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'service-details/:id', component: ServicesManagementComponent },
-  { path: 'customer', component: CustomerCreateAndEditComponent },
-  { path: 'company', component: CompanyCreateAndEditComponent },
-  { path: '', redirectTo: 'customer', pathMatch: 'full' },
+
+  { path: 'customerProfile', component: CustomerCreateAndEditComponent },
+  { path: 'companyProfile', component: CompanyCreateAndEditComponent },
+
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ];
-/*
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-prof-management
-*/
+
