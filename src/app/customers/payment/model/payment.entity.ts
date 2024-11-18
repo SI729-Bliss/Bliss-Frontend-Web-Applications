123bec1029f6edc9cd564c1a31b78ac9ed258b22
David Alejandro Rivas Sarango <u20191e831@upc.edu.pe>
2024-11-18 02:24:38 -0500

    feat: entity payment

--- src/app/customers/payment/model/payment.entity.ts
+++ src/app/customers/payment/model/payment.entity.ts
@@ -1,16 +1,21 @@
 export class Payment {
   id: number;
   date: string;
-  paymentMethod: string;
   amount: number;
-  ticketId: number;
+  paymentMethod: string;
+  status: string;
+  invoiceType: string;
+  reservationId: number;
   customerId: number;
 
   constructor(){
     this.id = 0;
     this.date = '';
-    this.paymentMethod = '';
     this.amount = 0;
-    this.ticketId = 0;
-    this.customerId = 0;}
+    this.paymentMethod = '';
+    this.status = '';
+    this.invoiceType = '';
+    this.reservationId = 0;
+    this.customerId = 0;
+  }
 }
