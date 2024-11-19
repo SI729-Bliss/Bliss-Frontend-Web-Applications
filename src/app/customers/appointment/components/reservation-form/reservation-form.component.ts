8ffa72ffb03b37553a075b6ed8380901e077f157
David Alejandro Rivas Sarango <u20191e831@upc.edu.pe>
2024-11-19 03:13:54 -0500

    feat: trying to create reservation from reservation-form

--- src/app/customers/appointment/components/reservation-form/reservation-form.component.ts
+++ src/app/customers/appointment/components/reservation-form/reservation-form.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
-import { ActivatedRoute, RouterModule } from '@angular/router';
+import { ActivatedRoute, RouterModule, Router } from '@angular/router';
 import { ReservationService } from '../../services/reservation.service';
 import { CompanyService } from '../../services/company.service';
 import { Service } from '../../model/service.entity';
+import { Reservation } from '../../model/reservation.entity';
 @Component({
   selector: 'app-reservation-form',
   standalone: true,
@@ -16,39 +17,62 @@ export class ReservationFormComponent implements OnInit {
   bookingForm: FormGroup;
   serviceName: string = '';
   companyName: string = '';
+  serviceId: number = 0;
+  companyId: number = 0;
 
   constructor(
     private fb: FormBuilder,
     private route: ActivatedRoute,
+    private router: Router,
     private reservationService: ReservationService,
     private companyService: CompanyService
   ) {
     this.bookingForm = this.fb.group({
       message: [''],
       bookingDate: ['', Validators.required],
-      bookingTime: ['', Validators.required]
+      bookingTime: ['', Validators.required],
+      totalAmount: [0, Validators.required]
     });
   }
 
   ngOnInit(): void {
     const serviceId = this.route.snapshot.queryParamMap.get('serviceId');
 
-    console.log('Service ID:', serviceId);
-
     if (serviceId) {
-      this.reservationService.getServiceById(+serviceId).subscribe(service => {
+      this.serviceId = +serviceId;
+      this.reservationService.getServiceById(this.serviceId).subscribe(service => {
         this.serviceName = service.name;
-        console.log('Service Name:', this.serviceName);
 
         const salonId = service.salonId;
-        console.log('Salon ID:', salonId);
 
         if (salonId) {
-          this.companyService.getCompanyById(+salonId).subscribe(company => {
+          this.companyId = +salonId;
+          this.companyService.getCompanyById(this.companyId).subscribe(company => {
             this.companyName = company.name;
-            console.log('Company Name:', this.companyName);
           });
         }
+      });
+    }
+  }
+
+  onSubmit(): void {
+    if (this.bookingForm.valid) {
+      const reservation: Reservation = {
+        id: 0,
+        customerId: 1, // Replace with actual customer ID
+        serviceId: this.serviceId, //3
+        companyId: this.companyId, //1
+        bookingDate: this.bookingForm.value.bookingDate,
+        bookingTime: this.bookingForm.value.bookingTime,
+        bookingStatus: 'false',
+        requirements: '',
+        message: this.bookingForm.value.message,
+        totalAmount: this.bookingForm.value.totalAmount
+      };
+
+      this.reservationService.create(reservation).subscribe(response => {
+        console.log('Reservation created:', response);
+        this.router.navigate(['/payment']);
       });
     }
   }
